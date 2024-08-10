fe5b5bac0039f54dc965ec6f49ef602b528241a3
Joshua Eatwell <josh-eatwell@hotmail.com>
2024-08-10 18:42:27 +0100

    Reading from a file
    Simple program that reads names from a file into an array, which is outputted into the console. The array is outputted again after it has sorted the names alphabetically.

---
+++ Arrayfromfile/Arrayfromfile/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+using System.IO;
+
+class ReadNameFromFile()
+{
+    
+    static void Main(string[] args)
+    {
+        
+        string[] readNames = File.ReadAllLines(@"C:\Users\josh-\Desktop\Git\CSharpConsoleApps\Arrayfromfile\Arrayfromfile\names\names.txt");
+        WriteLine("These are the names from the file: ");
+        foreach (string readName in readNames)
+            WriteLine("\t" + readName);
+
+        WriteLine("Number of names: " + readNames.Length);
+
+        Array.Sort(readNames);
+        WriteLine("Names have been sorted alphabetically");
+
+        foreach (string readName in readNames)
+            WriteLine("\n" + readName);    
+
+        return;
+
+
+    }
+}
