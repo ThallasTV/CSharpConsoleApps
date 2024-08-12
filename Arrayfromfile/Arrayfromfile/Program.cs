c3b5fa49d22ae169cb13424a47116455aa7a457a
Joshua Eatwell <josh-eatwell@hotmail.com>
2024-08-12 20:21:20 +0100

    outputting individual array element
    Simple way to output a specific array element that the user chooses

--- Arrayfromfile/Arrayfromfile/Program.cs
+++ Arrayfromfile/Arrayfromfile/Program.cs
@@ -6,6 +6,8 @@ class ReadNameFromFile()
     
     static void Main(string[] args)
     {
+        string? nameChoice = "";
+        int nameChoiceNumber;
         
         string[] readNames = File.ReadAllLines(@"C:\Users\josh-\Desktop\Git\CSharpConsoleApps\Arrayfromfile\Arrayfromfile\names\names.txt");
         WriteLine("These are the names from the file: ");
@@ -18,10 +20,29 @@ class ReadNameFromFile()
         WriteLine("Names have been sorted alphabetically");
 
         foreach (string readName in readNames)
-            WriteLine("\n" + readName);    
+            WriteLine("\n" + readName);
+
+        WriteLine("What name do you want to select? (e.g 5, 15).");
+        nameChoice = ReadLine();
+        bool stringInt = int.TryParse(nameChoice, out nameChoiceNumber);
+
+
+        for(int i = 0; i < nameChoiceNumber; i++)
+        {
+            int index = i;
+            index++;
+            if (index == nameChoiceNumber)
+            {
+                WriteLine("\t your selection: " + nameChoiceNumber);
+                WriteLine("\t Test" + readNames[i]);
+            }
+
+        }
 
         return;
 
-
+        
     }
+
+    
 }
